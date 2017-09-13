eec7ef1de87e7a8335a8622725f5161ba13d489b
Radovan Delic <infamouszgbgd@yahoo.com>
2017-09-13 17:18:50 +0200

    initialize

---
+++ js.js
@@ -0,0 +1,18 @@
+var cow = {
+    feet: 4, // d: This is number of feet ;|
+    color: 'brown', // d: This is the color;|
+    description: 'it moos' // d: it describes;|
+};
+
+var car = {
+    wheels: 4, // d: number of wheels;|
+    color: 'black', // d: describes colors;|
+    description: 'it takes you places' // d: it describes;|
+};
+
+var house = {
+    size: 150, // d: it defines the size;|
+    windows: 5, // d: describes the number of windows;|
+    floors: 2, // d: number of floors;|
+    description: "you live here" // d: it describes;|
+};
